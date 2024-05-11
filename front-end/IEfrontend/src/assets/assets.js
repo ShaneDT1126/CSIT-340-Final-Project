2913a344db85112d94d8ed4ffa01b3db50fec2d4
Meachelle <abellameachelleabella2988@gmail.com>
2024-05-11 05:09:37 -0400

    changed explore items

--- front-end/IEfrontend/src/assets/assets.js
+++ front-end/IEfrontend/src/assets/assets.js
@@ -2,14 +2,12 @@ import basket_icon from './basket_icon.png'
 import logo1 from './logo1.png'
 import header_img from './header_img.png'
 import search_icon from './search_icon.png'
-import menu_1 from './menu_1.png'
-import menu_2 from './menu_2.png'
-import menu_3 from './menu_3.png'
-import menu_4 from './menu_4.png'
-import menu_5 from './menu_5.png'
-import menu_6 from './menu_6.png'
-import menu_7 from './menu_7.png'
-import menu_8 from './menu_8.png'
+import menu_0 from './menu_all.png'
+import menu_1 from './menu_shirt.png'
+import menu_2 from './menu_bag.png'
+import menu_3 from './menu_jacket.png'
+import menu_4 from './menu_lanyard.png'
+
 
 import food_1 from './food_1.png'
 import food_2 from './food_2.png'
@@ -83,6 +81,10 @@ export const assets = {
 }
 
 export const menu_list = [
+    {
+        menu_name: "All",
+        menu_image: menu_0
+    },
     {
         menu_name: "Shirts",
         menu_image: menu_1
