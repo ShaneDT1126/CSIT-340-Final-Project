c8c55e7b517372912e08eea1a7a82eb997b8b675
Shijuchi <shanedelatorre2412@gmail.com>
2024-05-15 17:04:35 +0800

    implemented stripe

--- back-end/Backend/Backend/src/main/java/com/IEFinalProject/Backend/repository/PaymentRepo.java
+++ back-end/Backend/Backend/src/main/java/com/IEFinalProject/Backend/repository/PaymentRepo.java
@@ -2,9 +2,10 @@ package com.IEFinalProject.Backend.repository;
 
 import com.IEFinalProject.Backend.dto.PaymentReqRes;
 import com.IEFinalProject.Backend.model.Orders;
+import com.stripe.exception.StripeException;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PaymentRepo{
-    public PaymentReqRes createPaymentLink(Orders orders);
+    public PaymentReqRes createPaymentLink(Orders orders) throws StripeException;
 }
--- back-end/Backend/Backend/src/main/java/com/IEFinalProject/Backend/service/PaymentService.java
+++ back-end/Backend/Backend/src/main/java/com/IEFinalProject/Backend/service/PaymentService.java
@@ -4,6 +4,8 @@ import com.IEFinalProject.Backend.dto.PaymentReqRes;
 import com.IEFinalProject.Backend.model.Orders;
 import com.IEFinalProject.Backend.repository.PaymentRepo;
 import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.checkout.Session;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import com.stripe.param.checkout.SessionCreateParams;
@@ -15,17 +17,35 @@ public class PaymentService implements PaymentRepo {
     private String stripePrivateKey;
 
     @Override
-    public PaymentReqRes createPaymentLink(Orders orders) {
+    public PaymentReqRes createPaymentLink(Orders orders) throws StripeException {
 
-//        Stripe.apiKey=stripePrivateKey;
-//
-//        SessionCreateParams params = SessionCreateParams.builder()
-//                .addPaymentMethodType(SessionCreateParams.PaymentMethodType.CARD)
-//                .addPaymentMethodType(SessionCreateParams.PaymentMethodType.ACSS_DEBIT)
-//                .addPaymentMethodType(SessionCreateParams)
-//
-//                .build();
+        Stripe.apiKey=stripePrivateKey;
 
-        return null;
+        SessionCreateParams params = SessionCreateParams.builder()
+                .addPaymentMethodType(SessionCreateParams.PaymentMethodType.CARD)
+                .addPaymentMethodType(SessionCreateParams.PaymentMethodType.ACSS_DEBIT)
+                .addPaymentMethodType(SessionCreateParams.PaymentMethodType.ALIPAY)
+                .addPaymentMethodType(SessionCreateParams.PaymentMethodType.GRABPAY)
+                .setMode(SessionCreateParams.Mode.PAYMENT)
+                .setSuccessUrl("localhost:5173/")
+                .setCancelUrl("localhost:5173/")
+                .addLineItem(SessionCreateParams.LineItem.builder()
+                        .setQuantity(1L)
+                        .setPriceData(SessionCreateParams.LineItem.PriceData.builder()
+                                .setCurrency("php")
+                                .setUnitAmount( (long) orders.getTotalAmount()*100)
+                                .setProductData(SessionCreateParams.LineItem.PriceData.ProductData.builder()
+                                        .setName("Click Products")
+                                        .build()
+                                ).build()
+                        ).build()
+                ).build();
+
+        Session session = Session.create(params);
+
+        PaymentReqRes response = new PaymentReqRes();
+        response.setPayment_url(session.getUrl());
+
+        return response;
     }
 }
