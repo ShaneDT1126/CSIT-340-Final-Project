cf8b6c2cb53215f022b07d4c18a1b967d0767253
Meachelle <abellameachelleabella2988@gmail.com>
2024-05-15 16:06:01 +0800

    added contents of navbar and sidebar; responsive

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import Sidebar from './components/Sidebar/Sidebar'
+import Navbar from './components/Navbar/Navbar'
 const App = () => {
   return (
     <div>
-      
+      <Navbar />
+      <hr/>
+      <div className='app-content'>
+        <Sidebar />
+      </div>
     </div>
   )
 }
--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import './Sidebar.css'
+import { assets } from '../../assets/assets'
 
 const Sidebar = () => {
   return (
-    <div>
-      
+    <div className='sidebar'>
+      <div className="sidebar-options">
+        <div className="sidebar-option">
+            <img src={assets.add_icon} alt="" />
+            <p>Add Items</p>
+        </div>
+        <div className="sidebar-option">
+            <img src={assets.order_icon} alt="" />
+            <p>List Items</p>
+        </div><div className="sidebar-option">
+            <img src={assets.order_icon} alt="" />
+            <p>Orders</p>
+        </div>
+      </div>
     </div>
   )
 }
