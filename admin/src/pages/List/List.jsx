2ec907e403460ac7c28332b6e730c21b2a6e0cdb
Meachelle <abellameachelleabella2988@gmail.com>
2024-05-17 02:32:06 +0800

    added list and partial css

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './List.css'
+import axios from "axios"
+import {toast} from "react"
 
 const List = () => {
+  // const url = "http://localhost:4000"
+  // const [list,setList] = useState([]);
+
+  // const fetchList = async () => {
+  //   // const response = await axios.get('${url)/api/food/list');
+  //   console.log(response.data);
+  //   if (response.data.success) {
+  //     setList(response.data.data)
+  //   }
+  //   else {
+  //   toast.error("Error")
+  //   }
+  // }
+
+  //  useEffect(()=> {
+  //   fetchList();
+  //  }, [])
+
+
   return (
-    <div>
-      
+    <div className='list add flex-col'>
+      <p>All Foods List</p>
+      <div className="list-table">
+        <div className="list-table-format title">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+        {/* {list.map((item,index) => {
+          return (
+            <div key = {index} className='list-table-format'>
+                <img src = {'${url}/images'+item.image} alt='' />
+             </div> 
+          )
+        })} */}
+      </div>
     </div>
   )
 }
