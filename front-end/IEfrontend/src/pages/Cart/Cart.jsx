712b3340552334fceb2ad4ec0abcef3b8ed3a212
Shijuchi <shanedelatorre2412@gmail.com>
2024-05-28 00:51:30 +0800

    mapped total amount but didnt successfully map the whole cart, will continue later

--- front-end/IEfrontend/src/pages/Cart/Cart.jsx
+++ front-end/IEfrontend/src/pages/Cart/Cart.jsx
@@ -1,10 +1,61 @@
-import React, { useContext } from "react";
+import React, {useContext, useEffect, useState} from "react";
 import "./Cart.css";
 import { StoreContext } from "../../context/StoreContext";
 import { useNavigate } from "react-router-dom";
+import CartItemService from "../../service/CartItemService.js";
+import axios from "axios";
 const Cart = ({appUsername}) => {
-  const { cartItems, food_list, removeFromCart, getTotalCartAmount } = useContext(StoreContext);
+  // const { cartItems, food_list, removeFromCart, getTotalCartAmount } = useContext(StoreContext);
+  const [cartItem, setCartItem] = useState([]);
+  const [items, setItems] = useState([])
+  const [totalAmount, setTotalAmount] = useState(null);
   const navigate = useNavigate();
+
+  const fetchCartItems =  async () =>{
+    const token = localStorage.getItem('token')
+    try {
+      const cartItemsResponse = await CartItemService.getUserCart(appUsername, token);
+      const getTotalAmount = await CartItemService.getTotalAmount(appUsername, token);
+      setCartItem(cartItemsResponse.data|| [])
+      // setTotalAmount(getTotalAmount.data || 'N/A')
+      console.log(cartItemsResponse.data)
+    } catch (error){
+      console.error("Error fetching data: ", error)
+    }
+
+  };
+
+  const fetchItems = async () =>{
+    const token = localStorage.getItem('token')
+
+    try {
+      const response = await axios.get(`http://localhost:8080/user/getCart/${appUsername}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
+          });
+      const totalAmount = await axios.get(`http://localhost:8080/user/getCartTotal/${appUsername}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      setItems(response.data.cartItems || []);
+      setTotalAmount(totalAmount.data || 'No Items in Cart')
+      console.log(response.data)
+      console.log(totalAmount.data)
+    }catch (error){
+      console.log(error)
+    }
+  }
+
+
+
+  useEffect(() => {
+    fetchItems();
+  }, [appUsername]);
+
   return (
     <div className="cart">
       <div className="cart-items">
@@ -13,21 +64,19 @@ const Cart = ({appUsername}) => {
           <p>Title</p>
           <p>Price</p>
           <p>Quantity</p>
-          <p>Total</p>
           <p>Remove</p>
         </div>
         <br />
         <hr />
-        {food_list.map((item, index) => {
-          if (cartItems[item._id] > 0) {
+        {items.map((item, index) => {
+          if (items[item._id] > 0) {
             return (
               <div>
                 <div className="cart-items-title cart-items-item">
-                  <img src={item.image} alt="" />
-                  <p>{item.name}</p>
-                  <p>${item.price}</p>
-                  <p>{cartItems[item._id]}</p>
-                  <p>${item.price * cartItems[item._id]}</p>
+                  <img src={`http://localhost:8080/auth/getProductImage/${item.product.productId}`} alt={`${item.product.name}`} />
+                  <p>{item.product.name}</p>
+                  <p>{item.product.price}</p>
+                  <p>{item.quantity}</p>
                   <p onClick={() => removeFromCart(item._id)} className="cross">
                     x
                   </p>
@@ -44,17 +93,17 @@ const Cart = ({appUsername}) => {
           <div>
             <div className="cart-total-details">
               <p>Subtotal</p>
-              <p>${getTotalCartAmount()}</p>
+              <p>${totalAmount}</p>
             </div>
             <hr></hr>
             <div className="cart-total-details">
               <p>Delivery Fee</p>
-              <p>${getTotalCartAmount()===0?0:2}</p>
+              <p>${''}</p>
             </div>
             <hr></hr>
             <div className="cart-total-details">
               <b>Total</b>
-              <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
+              <b>${totalAmount}</b>
             </div>
           </div>
           <button onClick={()=>navigate('/order')}>PROCEED TO CHECKOUT</button>
--- front-end/IEfrontend/src/service/CartItemService.js
+++ front-end/IEfrontend/src/service/CartItemService.js
@@ -4,102 +4,6 @@ import axios from 'axios';
 class CartItemService {
     static BASE_URL = "http://localhost:8080"
 
-    static async login(username, password){
-        try{
-
-            const response = await axios.post(`${UserService.BASE_URL}/auth/login`, {username, password})
-            return response.data;
-
-        } catch (err){
-            throw err;
-        }
-    }
-
-    static async register(userData){
-        try{
-
-            const response = await axios.post(`${UserService.BASE_URL}/auth/userRegister`, userData)
-            return response.data;
-
-        } catch (err){
-            throw err;
-        }
-    }
-
-    static async getAllUsers(token){
-        try{
-
-            const response = await axios.get(`${UserService.BASE_URL}/admin/get-all-users`, 
-        {
-            headers: {Authorization: `Bearer ${token}`}
-        })
-
-            return response.data;
-
-        } catch (err){
-            throw err;
-        }
-    }
-
-    static async getYourProfile(token){
-        try{
-
-            const response = await axios.get(`${UserService.BASE_URL}/adminuser/get-profile`, 
-        {
-            headers: {Authorization: `Bearer ${token}`}
-        })
-
-            return response.data;
-
-        } catch (err){
-            throw err;
-        }
-    }
-
-    static async getUserById(userId, token){
-        try{
-
-            const response = await axios.get(`${UserService.BASE_URL}/admin/get-user/${userId}`, 
-        {
-            headers: {Authorization: `Bearer ${token}`}
-        })
-
-            return response.data;
-
-        } catch (err){
-            throw err;
-        }
-    }
-
-    static async deleteUserById(userId, token){
-        try{
-
-            const response = await axios.delete(`${UserService.BASE_URL}/admin/delete/${userId}`, 
-        {
-            headers: {Authorization: `Bearer ${token}`}
-        })
-
-            return response.data;
-
-        } catch (err){
-            throw err;
-        }
-    }
-
-    static async updateUser(userId, userData, token){
-        try{
-
-            const response = await axios.put(`${UserService.BASE_URL}/admin/update/${userId}`, userData,
-        {
-            headers: {Authorization: `Bearer ${token}`}
-        })
-
-            return response.data;
-
-        } catch (err){
-            throw err;
-        }
-    }
 
     static async addToCart(username, productId, quantity, token) {
         try {
@@ -116,30 +20,37 @@ class CartItemService {
         }
     };
 
-    static logout(){
-        localStorage.removeItem('token')
-        localStorage.removeItem('role')
-    }
+    static async getUserCart(username, token) {
+        try {
+            const response = await axios.get(`${CartItemService.BASE_URL}/user/getCart/${username}`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                }
+            );
 
-    static isAuthenticated(){
-        const token = localStorage.getItem('token')
-        return !!token
-    }
-    
-    static isAdmin(){
-        const role = localStorage.getItem('role')
-        return role === 'ADMIN'
-    }
+            return response.data;
 
-    static isUser(){
-        const role = localStorage.getItem('role')
-        return role === 'USER'
-    }
+        } catch (error){
+            throw error;
+        }
+    };
 
-    static adminOnly(){
-        return this.isAuthenticated() && this.isAdmin();
+    static async getTotalAmount(username, token) {
+        try {
+            const response = await axios.get(`${CartItemService.BASE_URL}/user/getCartTotal/${username}`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                }
+                );
+            return response.data;
+        } catch (error){
+            throw error;
+        }
     }
-
 
 }
 
